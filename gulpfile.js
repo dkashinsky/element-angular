510a44ed36ab2b5c8caf80019be6d144cbcc2277
Dmitry Kashinsky <dmitry.kashinsky@gmail.com>
2021-02-26 01:09:03 +0400

    build: fix publish build to copy necessary assets

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,37 @@ const gulp = require('gulp')
 const path = require('path')
 const fs = require('fs').promises
 
-gulp.task('assets', async () => {
-  const source = path.join(__dirname, './node_modules/element-ui/lib/theme-chalk/index.css')
-  const target = path.join(__dirname, './dist/element-angular/theme/index.css')
+const assets = [
+  {
+    src: './node_modules/element-ui/lib/theme-chalk/fonts/element-icons.ttf',
+    dest: './dist/element-angular/theme/fonts/element-icons.ttf'
+  },
+  {
+    src: './node_modules/element-ui/lib/theme-chalk/fonts/element-icons.woff',
+    dest: './dist/element-angular/theme/fonts/element-icons.woff'
+  },
+  {
+    src: './node_modules/element-ui/lib/theme-chalk/index.css',
+    dest: './dist/element-angular/theme/index.css'
+  },
+  {
+    src: './README.md',
+    dest: './dist/element-angular/README.md'
+  },
+  {
+    src: './LICENSE',
+    dest: './dist/element-angular/LICENSE'
+  }
+];
 
-  await fs.mkdir(path.dirname(target));
+const copyAsset = async ({ src, dest }) => {
+  const source = path.join(__dirname, src);
+  const target = path.join(__dirname, dest);
+
+  await fs.mkdir(path.dirname(target), { recursive: true });
   await fs.copyFile(source, target);
-})
+};
+
+gulp.task('copy-assets', async () => {
+  await Promise.all(assets.map(copyAsset));
+});
