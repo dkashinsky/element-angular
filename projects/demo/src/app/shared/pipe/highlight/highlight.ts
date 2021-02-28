35a002269d7affba625b1fe16592ecfb2704f676
Dmitry Kashinsky <dmitry.kashinsky@gmail.com>
2021-03-01 03:07:53 +0400

    chore(deps): update highlight

--- projects/demo/src/app/shared/pipe/highlight/highlight.ts
+++ projects/demo/src/app/shared/pipe/highlight/highlight.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core'
 import { SafeHtml, DomSanitizer } from '@angular/platform-browser'
-import * as hljs from 'highlight.js'
+import { highlightAuto } from 'highlight.js'
 
 @Pipe({ name: 'highlightAuto' })
 export class HighLightPipe implements PipeTransform {
@@ -11,7 +11,7 @@ export class HighLightPipe implements PipeTransform {
   }
   
   transform(value: string): SafeHtml {
-    const relevanceHandle: any = hljs.highlightAuto(value, ['shell', 'typescript', 'html', 'css'])
-    return this.sanitizer.bypassSecurityTrustHtml(relevanceHandle.value)
+    const result: AutoHighlightResult = highlightAuto(value, ['shell', 'typescript', 'html', 'css'])
+    return this.sanitizer.bypassSecurityTrustHtml(result.value)
   }
 }
