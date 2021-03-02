20bbf9436cbf22bc71910f7541526636071ced60
Dmitry Kashinsky <dmitry.kashinsky@gmail.com>
2021-03-03 00:21:36 +0400

    chore: remove localized docs folder

--- projects/demo/src/app/shared/services/docs/docs.service.ts
+++ projects/demo/src/app/shared/services/docs/docs.service.ts
@@ -1,4 +1,4 @@
-import { forwardRef, Inject, Injectable, LOCALE_ID } from '@angular/core'
+import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Observable, of } from 'rxjs'
 import { environment } from 'projects/demo/src/environments/environment'
@@ -6,22 +6,18 @@ import { environment } from 'projects/demo/src/environments/environment'
 @Injectable()
 export class DocsService {
   baseUrl: string = '/docs';
-  url: string
   
   constructor(
     private http: HttpClient,
-    @Inject(forwardRef(() => LOCALE_ID)) private locale: string,
   ) {
-    const path = this.locale === 'en-US' ? '/en-US' : ''
-    this.url = this.baseUrl + path;
   }
   
   getCatalog(): Observable<any> {
-    return this.http.get(`${this.url}/catalog.json`);
+    return this.http.get(`${this.baseUrl}/catalog.json`);
   }
   
   getDocuments(documentType: string): Observable<any> {
-    return this.http.get(`${this.url}/${documentType}.json`);
+    return this.http.get(`${this.baseUrl}/${documentType}.json`);
   }
   
   getVersion(): Observable<any> {
@@ -29,7 +25,7 @@ export class DocsService {
   }
   
   getChangeLogs(): Observable<any> {
-    return this.http.get(`${this.url}/changelog.json`);
+    return this.http.get(`${this.baseUrl}/changelog.json`);
   }
   
 }
